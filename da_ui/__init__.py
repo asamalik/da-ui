69454ce62827a9018029d2ecb7d4500ff6593127
Adam Samalik <asamalik@redhat.com>
2015-11-30 17:28:36 +0100

    views for the tree of runnables

--- da_ui/__init__.py
+++ da_ui/__init__.py
@@ -10,6 +10,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{0}'.format(os.path.join('..'
 db = SQLAlchemy(app)
 
 
+
+
+
 @app.route('/')
 def index():
     runables = ["Create a New Project", "Tweak an Existing Project", "Prepare an Environment", "Extras"]
@@ -17,6 +20,35 @@ def index():
     return flask.render_template('home.html', runables=runables, actions=actions)
 
 
-@app.route('/runnable/<section>/')
-def section(section):
-    return flask.render_template('section.html')
+@app.route('/section/')
+def section():
+    runables = ["Python", "Node.js", "Ruby", "PHP"]
+    return flask.render_template('section.html',
+        runables=runables,
+        title="Create a New Project",
+        description="Lorem ipsum dolor sit amet...")
+
+@app.route('/runnable/')
+def runnable():
+    runables = ["Python", "Node.js", "Ruby", "PHP"]
+    return flask.render_template('runnable.html',
+        runables=runables,
+        title="Create a New Project",
+        description="Lorem ipsum dolor sit amet...")
+
+@app.route('/running/')
+def running():
+    runables = ["Python", "Node.js", "Ruby", "PHP"]
+    return flask.render_template('running.html',
+        runables=runables,
+        title="Create a New Project",
+        description="Lorem ipsum dolor sit amet...")
+
+@app.route('/finished/')
+def finished():
+    runables = ["Python", "Node.js", "Ruby", "PHP"]
+    return flask.render_template('finished.html',
+        runables=runables,
+        title="Create a New Project",
+        description="Lorem ipsum dolor sit amet...")
+
